908fa835a8a4dd512c9ef2660feb3a4357a459ef
3140102441 <2435679564@qq.com>
2019-04-28 10:41:50 +0800

    update

--- src/lr_schedule.py
+++ src/lr_schedule.py
@@ -13,11 +13,12 @@ def inv_lr_scheduler(param_lr, optimizer, iter_num, gamma, power, init_lr=0.001)
 def step_lr_scheduler(param_lr, optimizer, iter_num, gamma, step, init_lr=0.001):
     """Decay learning rate by a factor of 0.1 every lr_decay_epoch epochs."""
     lr = init_lr * (gamma ** (iter_num // step))
-    if iter_num % step == 0:
-        i=0
-        for param_group in optimizer.param_groups:
-            param_group['lr'] = gamma * param_lr[i]
-            i+=1
+
+    i=0
+    for param_group in optimizer.param_groups:
+        param_group['lr'] = lr * param_lr[i]
+        i+=1
+
     return optimizer
 
 
